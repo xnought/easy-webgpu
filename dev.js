622fd5fd35f5fff9c8a14647942b125ea32ff93b
xnought <drbertucci@hotmail.com>
2025-01-25 16:03:51 -0500

    basic malloc

--- dev.js
+++ dev.js
@@ -1,2 +1,2 @@
 import { dev } from "./src";
-dev();
+dev().then((d) => {});
--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
+function assert(truth, msg = "ASSERT FAILED") {
+	if (!truth) throw new Error(msg);
+}
+
 // AIM TO MIMIC PyCUDA https://homepages.math.uic.edu/~jan/mcs572f16/mcs572notes/lec29.html
 class GPU {
-	mem_alloc() {}
+	constructor(device) {
+		this.device = device;
+	}
+	static async init() {
+		const adapter = await navigator.gpu.requestAdapter();
+		assert(adapter, "adapter exists");
+		const device = await adapter.requestDevice();
+		assert(device, "device exists");
+		return new GPU(device);
+	}
+	mem_alloc(bytes) {
+		assert(bytes > 0);
+		const buffer = this.device.createBuffer({
+			size: bytes,
+			usage: GPUBufferUsage.STORAGE,
+		});
+		return buffer;
+	}
 	memcpy_dtoh() {}
 	memcpy_htod() {}
+	free(buffer) {
+		buffer.destroy();
+	}
 }
 
 class SourceModule {
@@ -13,25 +37,26 @@ class SourceModule {
 	get_function() {}
 }
 
-export function dev() {
+export async function dev() {
 	const a = new Float32Array([1, 2, 3]);
 	const b = new Float32Array([3, 2, 1]);
 	const c = new Float32Array([0]);
 
-	const gpu = new GPU();
+	const gpu = await GPU.init();
 	const aGPU = gpu.mem_alloc(a.byteLength);
-	const bGPU = gpu.mem_alloc(b.byteLength);
-	const cGPU = gpu.mem_alloc(c.byteLength);
+	gpu.free(aGPU);
+	// const bGPU = gpu.mem_alloc(b.byteLength);
+	// const cGPU = gpu.mem_alloc(c.byteLength);
 
-	gpu.memcpy_htod(aGPU, a);
-	gpu.memcpy_htod(bGPU, b);
-	gpu.memcpy_htod(cGPU, c);
+	// gpu.memcpy_htod(aGPU, a);
+	// gpu.memcpy_htod(bGPU, b);
+	// gpu.memcpy_htod(cGPU, c);
 
-	// Then perform computation
+	// // Then perform computation
 	// const mod = new SourceModule(gpu, `some kernel function here`);
 	// const func = mod.get_function("dot");
 	// func(aGPU, bGPU, cGPU);
 
 	// grab the result from cGPU and put into c
-	gpu.memcpy_dtoh(c, cGPU);
+	// gpu.memcpy_dtoh(c, cGPU);
 }
