f2204e81a80e597368914b253713eed8e4806e0f
xnought <drbertucci@hotmail.com>
2025-01-26 16:10:05 -0500

    zeros and ones 1d vector

--- src/tensor.js
+++ src/tensor.js
@@ -11,6 +11,16 @@ export class Tensor {
 		gpu.memcpyHostToDevice(this.data, typedArrayData);
 		this.dtype = typedArrayData.constructor;
 	}
+
+	static fill(gpu, length, fillValue, dtype) {
+		return new Tensor(gpu, new dtype(length).fill(fillValue));
+	}
+	static zeros(gpu, length, dtype) {
+		return Tensor.fill(gpu, length, 0, dtype);
+	}
+	static ones(gpu, length, dtype) {
+		return Tensor.fill(gpu, length, 1, dtype);
+	}
 	async print() {
 		console.log(`Tensor(this.dtype=${this.dtype.name}, `);
 		await this.gpu.printGPUBuffer(this.data, "this.data=", this.dtype);
@@ -24,7 +34,8 @@ export class Tensor {
 
 export async function dev() {
 	const gpu = await GPU.init();
-	const a = new Tensor(gpu, new Float32Array([1, 2, 3]));
+	// const a = new Tensor(gpu, new Float32Array([1, 2, 3, 4]));
+	const a = Tensor.ones(gpu, 16, Float32Array);
 	await a.print();
 	a.free();
 }
