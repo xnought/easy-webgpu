57765534a4d48ce1964b6354df2e60314da3db6e
xnought <drbertucci@hotmail.com>
2025-01-25 23:01:48 -0500

    source module execution

--- src/index.js
+++ src/index.js
@@ -74,21 +74,17 @@ class GPU {
 	 */
 	execKernel({ code, buffers, dispatchWorkgroups, entryPoint = "main" }) {
 		const mod = this.device.createShaderModule({ code });
-		const bindGroupLayout = this.device.createBindGroupLayout({
-			entries: parseLayout(buffers),
-		});
-		const bindGroup = this.device.createBindGroup({
-			layout: bindGroupLayout,
-			entries: parseEntries(buffers),
-		});
 		const computePipeline = this.device.createComputePipeline({
-			layout: this.device.createPipelineLayout({
-				bindGroupLayouts: [bindGroupLayout],
-			}),
+			layout: "auto",
 			compute: {
 				module: mod,
 				entryPoint,
 			},
+		});
+		const bindGroupLayout = computePipeline.getBindGroupLayout(0);
+		const bindGroup = this.device.createBindGroup({
+			layout: bindGroupLayout,
+			entries: parseEntries(buffers),
 		});
 		const commandEncoder = this.device.createCommandEncoder();
 		const passEncoder = commandEncoder.beginComputePass();
@@ -97,6 +93,62 @@ class GPU {
 		passEncoder.dispatchWorkgroups(...dispatchWorkgroups);
 		passEncoder.end();
 		this.device.queue.submit([commandEncoder.finish()]);
+	}
+
+	SourceModule(kernel) {
+		return new SourceModule(this, kernel);
+	}
+}
+
+function parseLayout(buffers) {
+	return buffers.map((d, i) => {
+		return {
+			binding: i,
+			visibility: GPUShaderStage.COMPUTE,
+			buffer: { type: d.type ?? "read-only-storage" },
+		};
+	});
+}
+
+function parseEntries(buffers) {
+	return buffers.map((d, i) => {
+		return { binding: i, resource: { buffer: d.buffer } };
+	});
+}
+
+class SourceModule {
+	constructor(gpu, kernel) {
+		this.gpu = gpu;
+		this.device = gpu.device;
+		this.kernel = kernel;
+	}
+	getFunction(name) {
+		const mod = this.device.createShaderModule({ code: this.kernel });
+		const computePipeline = this.device.createComputePipeline({
+			layout: "auto",
+			compute: {
+				module: mod,
+				entryPoint: name,
+			},
+		});
+		const bindGroupLayout = computePipeline.getBindGroupLayout(0);
+		return ({ inputs, workgroups }) => {
+			assert(inputs !== undefined);
+			assert(workgroups !== undefined);
+
+			const bindGroup = this.device.createBindGroup({
+				layout: bindGroupLayout,
+				entries: inputs,
+			});
+			const commandEncoder = this.device.createCommandEncoder();
+			const passEncoder = commandEncoder.beginComputePass();
+			passEncoder.setPipeline(computePipeline);
+			passEncoder.setBindGroup(0, bindGroup);
+			passEncoder.dispatchWorkgroups(...workgroups);
+			passEncoder.end();
+
+			this.device.queue.submit([commandEncoder.finish()]);
+		};
 	}
 }
 
@@ -120,36 +172,21 @@ async function testMemAllocAndCopy() {
 		same,
 		"[testMemAllocAndCopy] Data not copied to or from GPU correctly."
 	);
+	console.log("[testMemAllocAndCopy] PASSED");
 
 	gpu.free(cGPU);
-}
-
-function parseLayout(buffers) {
-	return buffers.map((d, i) => {
-		return {
-			binding: i,
-			visibility: GPUShaderStage.COMPUTE,
-			buffer: { type: d.type ?? "read-only-storage" },
-		};
-	});
-}
-
-function parseEntries(buffers) {
-	return buffers.map((d, i) => {
-		return { binding: i, resource: { buffer: d.buffer } };
-	});
 }
 
 export async function test() {
 	await testMemAllocAndCopy();
 	await testSingleWorkgroup();
-	console.log("ALL TESTS PASSED");
+	await testSourceModule();
 }
 
 async function testSingleWorkgroup() {
 	const gpu = await GPU.init();
 
-	const length = 256;
+	const length = 128;
 	const cpuA = new Float32Array(length).fill(0).map((_) => Math.random());
 	const cpuB = new Float32Array(length).fill(0).map((_) => Math.random());
 	const cpuC = new Float32Array([0]);
@@ -203,14 +240,82 @@ async function testSingleWorkgroup() {
 	}, 0);
 
 	assert(
-		actual.toFixed(1) === cpuC[0].toFixed(1),
-		"[] Data not copied to or from GPU correctly."
+		actual.toFixed(0) === cpuC[0].toFixed(0),
+		"[testSingleWorkgroup] incorrect dot product"
 	);
+	console.log("[testSingleWorkgroup] PASSED");
+
+	gpu.free(gpuA);
+	gpu.free(gpuB);
+	gpu.free(gpuC);
+}
+async function testSourceModule() {
+	const gpu = await GPU.init();
+
+	const length = 256;
+	const cpuA = new Float32Array(length).fill(0).map((_) => Math.random());
+	const cpuB = new Float32Array(length).fill(0).map((_) => Math.random());
+	const cpuC = new Float32Array([0]);
+
+	const gpuA = gpu.memAlloc(cpuA.byteLength);
+	const gpuB = gpu.memAlloc(cpuB.byteLength);
+	const gpuC = gpu.memAlloc(cpuC.byteLength);
+
+	gpu.memcpyHostToDevice(gpuA, cpuA);
+	gpu.memcpyHostToDevice(gpuB, cpuB);
+	gpu.memcpyHostToDevice(gpuC, cpuC);
+
+	const THREADS_PER_BLOCK = 256;
+	const mod = gpu.SourceModule(`
+      @group(0) @binding(0) var<storage, read> a: array<f32>;
+      @group(0) @binding(1) var<storage, read> b: array<f32>;
+      @group(0) @binding(2) var<storage, read_write> c: f32;
+
+      var<workgroup> partialSums: array<f32, ${THREADS_PER_BLOCK}>;
+
+      @compute @workgroup_size(${THREADS_PER_BLOCK})
+      fn myDot(@builtin(global_invocation_id) gid : vec3u, @builtin(local_invocation_id) lid : vec3u) {
+        if(gid.x < ${length}) {
+          partialSums[lid.x] += a[gid.x]*b[gid.x];
+        }
+
+        workgroupBarrier();
+        if(lid.x == 0) {
+          var summed: f32 = 0.0;
+          for(var i: u32 = 0; i < ${THREADS_PER_BLOCK}; i++) {
+            summed += partialSums[i];
+          }
+          c += summed;
+        }
+      }
+    `);
+	const dot = mod.getFunction("myDot");
+	dot({
+		inputs: [
+			{ binding: 0, resource: { buffer: gpuA } },
+			{ binding: 1, resource: { buffer: gpuB } },
+			{ binding: 2, resource: { buffer: gpuC } },
+		],
+		workgroups: [1],
+	});
+
+	// copy back the result and compare
+	await gpu.memcpyDeviceToHost(cpuC, gpuC);
+	let actual = cpuA.reduce((acc, _, i) => {
+		return acc + cpuA[i] * cpuB[i];
+	}, 0);
+
+	assert(
+		actual.toFixed(0) === cpuC[0].toFixed(0),
+		"[testSourceModule] incorrect dot product"
+	);
+	console.log("[testSourceModule] PASSED");
+
 	gpu.free(gpuA);
 	gpu.free(gpuB);
 	gpu.free(gpuC);
 }
 
 export async function dev() {
-	await test();
+	await testSourceModule();
 }
