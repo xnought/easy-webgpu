df7883137df282c39f401b81de6ebc71d3c5fe98
xnought <drbertucci@hotmail.com>
2025-01-26 20:44:58 -0500

    reorg

--- example/index.js
+++ example/index.js
@@ -1,4 +1,11 @@
-import { GPU } from "../webgpu-compute";
+import { GPU, assert } from "../webgpu-compute";
+
+main();
+
+async function main() {
+	await test();
+	await example();
+}
 
 async function example() {
 	const gpu = await GPU.init();
@@ -36,4 +43,163 @@ async function example() {
 	console.log(cpuData); // > [1, 4, 9, 16]
 }
 
-example();
+async function testMemAllocAndCopy() {
+	const gpu = await GPU.init();
+	const c = new Float32Array([1.0, 2.0, 3.0]);
+	const result = new Float32Array(c.length);
+
+	const cGPU = gpu.memAlloc(c.byteLength);
+	gpu.memcpyHostToDevice(cGPU, c);
+
+	await gpu.memcpyDeviceToHost(result, cGPU);
+	let same = true;
+	for (let i = 0; i < c.length; i++) {
+		if (c[i] !== result[i]) {
+			same = false;
+			break;
+		}
+	}
+	assert(
+		same,
+		"[testMemAllocAndCopy] Data not copied to or from GPU correctly."
+	);
+	console.log("[testMemAllocAndCopy] PASSED");
+
+	gpu.free(cGPU);
+}
+
+async function testSingleWorkgroup() {
+	const gpu = await GPU.init();
+
+	const length = 128;
+	const cpuA = new Float32Array(length).fill(0).map((_) => Math.random());
+	const cpuB = new Float32Array(length).fill(0).map((_) => Math.random());
+	const cpuC = new Float32Array([0]);
+
+	const gpuA = gpu.memAlloc(cpuA.byteLength);
+	const gpuB = gpu.memAlloc(cpuB.byteLength);
+	const gpuC = gpu.memAlloc(cpuC.byteLength);
+
+	gpu.memcpyHostToDevice(gpuA, cpuA);
+	gpu.memcpyHostToDevice(gpuB, cpuB);
+	gpu.memcpyHostToDevice(gpuC, cpuC);
+
+	const THREADS_PER_BLOCK = 256;
+	const mod = gpu.SourceModule(/*wgsl*/ `
+      @group(0) @binding(0) var<storage, read> a: array<f32>;
+      @group(0) @binding(1) var<storage, read> b: array<f32>;
+      @group(0) @binding(2) var<storage, read_write> c: f32;
+
+      var<workgroup> partialSums: array<f32, ${THREADS_PER_BLOCK}>;
+
+      @compute @workgroup_size(${THREADS_PER_BLOCK})
+      fn myDot(@builtin(global_invocation_id) gid : vec3u, @builtin(local_invocation_id) lid : vec3u) {
+        if(gid.x < ${length}) {
+          partialSums[lid.x] += a[gid.x]*b[gid.x];
+        }
+
+        workgroupBarrier();
+        if(lid.x == 0) {
+          var summed: f32 = 0.0;
+          for(var i: u32 = 0; i < ${THREADS_PER_BLOCK}; i++) {
+            summed += partialSums[i];
+          }
+          c += summed;
+        }
+      }
+    `);
+	const dot = mod.getFunction("myDot", true);
+	dot(
+		[1],
+		{ binding: 0, resource: { buffer: gpuA } },
+		{ binding: 1, resource: { buffer: gpuB } },
+		{ binding: 2, resource: { buffer: gpuC } }
+	);
+
+	// copy back the result and compare
+	await gpu.memcpyDeviceToHost(cpuC, gpuC);
+	let actual = cpuA.reduce((acc, _, i) => {
+		return acc + cpuA[i] * cpuB[i];
+	}, 0);
+
+	assert(
+		actual.toFixed(0) === cpuC[0].toFixed(0),
+		"[testSingleWorkgroup] incorrect dot product"
+	);
+	console.log("[testSingleWorkgroup] PASSED");
+
+	gpu.free(gpuA);
+	gpu.free(gpuB);
+	gpu.free(gpuC);
+}
+
+async function testOtherTypes() {
+	const gpu = await GPU.init();
+
+	const length = 256;
+	const cpuA = new Uint32Array(length).fill(0).map((_, i) => i);
+	const cpuB = new Uint32Array(length).fill(1);
+	const cpuC = new Uint32Array([0]);
+	const cpuN = new Uint32Array([length]);
+
+	const gpuA = gpu.memAlloc(cpuA.byteLength);
+	const gpuB = gpu.memAlloc(cpuB.byteLength);
+	const gpuC = gpu.memAlloc(cpuC.byteLength);
+	const gpuN = gpu.memAlloc(cpuN.byteLength);
+
+	gpu.memcpyHostToDevice(gpuA, cpuA);
+	gpu.memcpyHostToDevice(gpuB, cpuB);
+	gpu.memcpyHostToDevice(gpuC, cpuC);
+	gpu.memcpyHostToDevice(gpuN, cpuN);
+
+	const THREADS_PER_BLOCK = 256;
+	const mod = gpu.SourceModule(/*wgsl*/ `
+      @group(0) @binding(0) var<storage, read> a: array<u32>;
+      @group(0) @binding(1) var<storage, read> b: array<u32>;
+      @group(0) @binding(2) var<storage, read_write> c: u32;
+      @group(0) @binding(3) var<storage, read> n: u32;
+
+	  var<workgroup> partialSums: array<u32, ${THREADS_PER_BLOCK}>;
+
+      @compute @workgroup_size(${THREADS_PER_BLOCK})
+      fn myDot(@builtin(global_invocation_id) gid : vec3u, @builtin(local_invocation_id) lid : vec3u) {
+
+        if(gid.x < n) {
+          partialSums[lid.x] = a[gid.x]*b[gid.x];
+        }
+        workgroupBarrier();
+
+        if(lid.x == 0) {
+          var summed: u32 = 0;
+          for(var i: u32 = 0; i < ${THREADS_PER_BLOCK}; i++) {
+            summed += partialSums[i];
+          }
+		  c += summed;
+        }
+      }
+    `);
+	const dot = mod.getFunction("myDot");
+	dot([1], gpuA, gpuB, gpuC, gpuN);
+
+	// copy back the result and compare
+	await gpu.memcpyDeviceToHost(cpuC, gpuC);
+	let actual = cpuA.reduce((acc, _, i) => {
+		return acc + cpuA[i] * cpuB[i];
+	}, 0);
+
+	assert(
+		actual.toFixed(0) === cpuC[0].toFixed(0),
+		"[testOtherTypes] incorrect dot product"
+	);
+	console.log("[testMultipleWorkgroups] PASSED");
+
+	gpu.free(gpuA);
+	gpu.free(gpuB);
+	gpu.free(gpuC);
+}
+
+async function test() {
+	await testMemAllocAndCopy();
+	await testSingleWorkgroup();
+	await testOtherTypes();
+}
